790733d3980d1d5630820d1abda49ef7803fcdc5
MarioLFB <popesco19@hotmail.com>
2024-01-26 18:47:49 +0000

    Create an EventListener to interact with the questions and display in the console whether the answer is correct or incorrect.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,17 @@ function displayQuestion(q) { //Function to display the question and alternative
     let divTitle = document.getElementById("title");
    divTitle.textContent = q.title;
 
-    let divAlternatives = document.querySelectorAll(".question");
-   divAlternatives.forEach(function(element, index){
-         element.textContent = q.alternatives[index];
+    let divAlternatives = document.querySelectorAll(".question"); //Selecting all the elements with the class "question" and putting them in a variable.
+   divAlternatives.forEach(function(element, index){ //Looping through the elements in the variable "divAlternatives" and putting them in the variable "element".
+         element.textContent = q.alternatives[index]; //Putting the text content of the elements in the variable "divAlternatives" in the variable "element".
+         element.addEventListener("click", function(){ //Adding an event listener to the variable "element" with the event "click" and a function as argument.
+            if(index == q.answer){ //If the index of the variable "element" is equal to the answer in the variable "question" then the console will log "Correct!".
+                console.log("Correct!"); //If the index of the variable "element" is not equal to the answer in the variable "question" then the console will log "Wrong!".
+            } else {
+                console.log("Wrong!"); //If the index of the variable "element" is not equal to the answer in the variable "question" then the console will log "Wrong!".
+            }
+
+        })
     })
 }
 
