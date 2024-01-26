a2d2f244bf65b83d54cf9848eb407ac39499016f
MarioLFB <popesco19@hotmail.com>
2024-01-26 17:41:34 +0000

    Create the function displayQuestion. Create a link to call the function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,9 @@ let question = { //Variable "question" from the quiz.
     answer: 2,
 }
 
+function displayQuestion(q) { //Function to display the question and alternatives.
+   let divTitle = document.getElementById("title");
+   divTitle.textContent = q.title;
+}
+
+displayQuestion(question); //Calling the function "displayQuestion" with the variable "question" as argument.
