e553de80b96561a262b91e1ddb97b6675dc9ae96
MarioLFB <popesco19@hotmail.com>
2024-01-26 18:14:13 +0000

    Create within the displayQuestion function the let divAlternatives. Get all the elements with the class "question." Create a forEach loop to iterate through the alternatives array.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 let question = { //Variable "question" from the quiz.
-    title: 'What is the capital of Japan?',
+    title: "What is the capital of Japan?",
     alternatives: ["Beijing", "Seoul", "Tokyo", "Bangkok"],
     answer: 2,
 }
 
 function displayQuestion(q) { //Function to display the question and alternatives.
-   let divTitle = document.getElementById("title");
+    let divTitle = document.getElementById("title");
    divTitle.textContent = q.title;
+
+    let divAlternatives = document.querySelectorAll(".question");
+   divAlternatives.forEach(function(element, index){
+         element.textContent = q.alternatives[index];
+    })
 }
 
 displayQuestion(question); //Calling the function "displayQuestion" with the variable "question" as argument.
