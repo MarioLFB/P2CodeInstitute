3595d9181cdfc73124e107192ad4fc56514aeaf9
MarioLFB <popesco19@hotmail.com>
2024-01-26 19:44:28 +0000

    Fix function start. Create function checkAnswer to check if its correct or wrong displaying in the console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,37 @@
-let question = { //Variable "question" from the quiz.
+let question = {
     title: "What is the capital of Japan?",
     alternatives: ["Beijing", "Seoul", "Tokyo", "Bangkok"],
     answer: 2,
-}
+};
 
 function start() {
+    let divAlternatives = document.querySelectorAll(".question");
+    divAlternatives.forEach((element, index) => {
+        element.addEventListener("click", () => {
+            checkAnswer(question, index);
+        });
+    });
 
-    let divAlternatives = document.querySelectorAll(".question"); //Selecting all the elements with the class "question" and putting them in a variable.
-    divAlternatives.forEach(function(element, index){ //Looping through the elements in the variable "divAlternatives" and putting them in the variable "element".
-          element.addEventListener("click", function(){ //Adding an event listener to the variable "element" with the event "click" and a function as argument.
-           console.log("check answer")
-         })
-     })
-
-
-    displayQuestion(question); //Calling the function "displayQuestion" with the argument "question".
-
-} 
-
-
-
-
-function displayQuestion(q) { //Function to display the question and alternatives.
-    let divTitle = document.getElementById("title");
-   divTitle.textContent = q.title;
-
-    let divAlternatives = document.querySelectorAll(".question"); //Selecting all the elements with the class "question" and putting them in a variable.
-   divAlternatives.forEach(function(element, index){ //Looping through the elements in the variable "divAlternatives" and putting them in the variable "element".
-         element.textContent = q.alternatives[index]; //Putting the text content of the elements in the variable "divAlternatives" in the variable "element".
-         
-    })
+    displayQuestion(question);
 }
 
-start(); //Calling the function "start".
+function displayQuestion(q) {
+    let divTitle = document.getElementById("title");
+    divTitle.textContent = q.title;
+
+    let divAlternatives = document.querySelectorAll(".question");
+    divAlternatives.forEach(function (element, index) {
+        element.textContent = q.alternatives[index];
+    });
+}
+
+function checkAnswer(q, user) {
+    if (q.answer == user) {
+        console.log("Correct");
+    } else {
+        console.log("Wrong");
+    }
+}
+
+start();
+
