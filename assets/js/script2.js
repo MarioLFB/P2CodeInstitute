afb19ee32f1420f38f712e92279376539fffb157
MarioLFB <popesco19@hotmail.com>
2024-01-30 19:25:45 +0000

    Added input validation for 'Start Quiz' button in script2.js. Checks if 'name' input is filled before redirecting to 'puzzle.html', prompts user to enter name if field is empty.

---
+++ assets/js/script2.js
@@ -0,0 +1,18 @@
+// JavaScript code in script2.js: Adds an event listener to the 'Start Quiz' button. If the associated input field ('name') is filled,
+// it redirects to the 'puzzle.html' page; otherwise, it prompts the user to fill in their name before starting the quiz.
+
+document.addEventListener('DOMContentLoaded', function () {
+    const startButton = document.querySelector('#quizForm button');
+
+    if (startButton) {
+        startButton.addEventListener('click', function () {
+            const nameInput = document.getElementById('name');
+        
+            if (nameInput.value.trim() !== '') {
+                window.location.href = 'puzzle.html';
+            } else {
+                alert('Please fill in your name before starting the quiz.');
+            }
+        });
+    }
+});
