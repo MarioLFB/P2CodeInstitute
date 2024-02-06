8f228ad829c6b6c97797dcd03c8a7593ac7d3dd7
MarioLFB <popesco19@hotmail.com>
2024-02-06 21:21:38 +0000

    Add EmailJS API Script. Add form EmailJS. Add style to form in contactus.css. Add emailJS script to script3.js

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -36,3 +36,24 @@ const photoComments = [
     }
 
     createPhotoCarouselLoop(photoComments);
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_srmqilh';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
